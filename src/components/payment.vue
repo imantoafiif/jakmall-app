159bc6900f03cf9979b1a03961951e8520396ba1
imantoafiif <imantoafiif@gmail.com>
2024-03-06 16:53:17 +0700

    develop payment

--- src/components/payment.vue
+++ src/components/payment.vue
@@ -11,7 +11,24 @@
         <div class="payment-container__item">
             <Title :label="'Shipment'"></Title>
             <div class="payment-container__item__list">
-                <div>
+                <div
+                    :class="{'payment-container__item__list__active': item.method == 'GO-JEK'}" 
+                    v-for="item in [
+                        { method: 'GO-JEK', price: 15000 },
+                        { method: 'JNE', price: 9000 },
+                        { method: 'Personal Courier', price: 29000 },
+                    ]">
+                    <span>{{ item.method }}</span>
+                    <strong>{{ item.price }}</strong>
+                </div>
+            </div>
+        </div>
+        <div class="payment-container__item">
+            <Title :label="'Payment'"></Title>
+            <div class="payment-container__item__list">
+                <div
+                    :class="{'payment-container__item__list__active': i == 1}" 
+                    v-for="i in 3">
 
                 </div>
             </div>
@@ -35,9 +52,26 @@
             &__list 
                 display flex
                 flex-direction row
+                gap 15px
 
-                & > div 
+                & > div
                     width 180px
                     height 60px
-                    border 2px solid #1BD97B
+                    border 1px solid #CCCCCC
+                    cursor pointer
+                    display flex
+                    flex-direction column
+                    justify-content center
+                    font-weight 500
+                    padding 0 10px
+
+                    & > strong
+                        font-size 16px
+
+                &__active
+                    border 2px solid #1BD97B !important
+                    background #1BD97B1A
+
+            &:last-child
+                margin-top 30px
 </style>
